b0f87dc812eb9a4c148de04886405ab3eb871199
Vahtee Boo <vahtee.boo@gmail.com>
2022-02-11 14:15:50 +0200

    Initial Commit

---
+++ CoolToDoList/CoolToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoolToDoListApp.swift
+//  CoolToDoList
+//
+//  Created by Vahtee Boo on 11.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoolToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
